ded44b63397e1f30374b0c71d49fa043325b1094
Marlon Savian <marlon.savian@gmail.com>
2020-10-07 07:58:18 -0300

    resume

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Marlon Savian</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              {/* <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li> */}
            </ul>
 
         </div>
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -25,7 +25,7 @@ class Resume extends Component {
     return (
       <section id="resume">
 
-      <div className="row education">
+      {/* <div className="row education">
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
          </div>
@@ -37,13 +37,13 @@ class Resume extends Component {
                </div>
             </div>
          </div>
-      </div>
+      </div> */}
 
 
       <div className="row work">
 
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>Work Experience</span></h1>
          </div>
 
          <div className="nine columns main-col">
